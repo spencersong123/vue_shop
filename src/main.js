9711678229884eb502d85379683d9944f3e05d34
spencersong <1028495632@qq.com>
2021-05-03 22:39:07 +0800

    all login

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import'element-ui/lib/theme-chalk/index.css';
+import "./assets/global.css"
+import ElementUI from 'element-ui';
+import axios from "axios";
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
+Vue.prototype.$http = axios;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+Vue.use(ElementUI);
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
---
+++ src/views/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        <el-button type="info" @click="logout">登出</el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods:{
+            logout(){
+                window.sessionStorage.clear();
+                this.$router.push("/");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
